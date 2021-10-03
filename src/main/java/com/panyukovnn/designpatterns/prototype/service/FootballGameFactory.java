d3170a105337c8dbd7c6ee8137a4b04c92fdd25d
Nickolay <zylex77@gmail.com>
2021-10-03 14:27:52 +0300

    add bulder and prototype

---
+++ src/main/java/com/panyukovnn/designpatterns/prototype/service/FootballGameFactory.java
@@ -0,0 +1,17 @@
+package com.panyukovnn.designpatterns.prototype.service;
+
+import com.panyukovnn.designpatterns.prototype.model.ClonableGame;
+import com.panyukovnn.designpatterns.prototype.model.FootballGame;
+
+public class FootballGameFactory {
+
+    private final FootballGame game;
+
+    public FootballGameFactory(FootballGame game) {
+        this.game = game;
+    }
+
+    public FootballGame cloneGame() {
+        return game.copy();
+    }
+}
