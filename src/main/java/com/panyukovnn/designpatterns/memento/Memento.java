79b8b7c46ac19a3feb1a6d77c2b304a1061952a2
Nickolay <zylex77@gmail.com>
2021-10-19 22:37:14 +0300

    add memento

---
+++ src/main/java/com/panyukovnn/designpatterns/memento/Memento.java
@@ -0,0 +1,41 @@
+package com.panyukovnn.designpatterns.memento;
+
+/**
+ * Цель - сохранение внутреннего состояния объекта за его пределами
+ *
+ * Позволяет запоминать состояние объекта за его пределами не нарушая инкапсуляцию, а потом восстанавливать
+ * в случае необходимости
+ */
+public class Memento {
+
+    public static void main(String[] args) throws InterruptedException {
+        Project project = new Project();
+        GithubRepo githubRepo = new GithubRepo();
+
+        System.out.println("Creating new project. Version 1.0");
+        project.setVersionAndDate("Version 1.0");
+
+        System.out.println(project);
+        System.out.println();
+
+        System.out.println("Saving current version to github...");
+        githubRepo.setSave(project.save());
+
+        System.out.println("Updating project to version 1.1");
+        System.out.println("Writing bad code...");
+        Thread.sleep(2000);
+
+        project.setVersionAndDate("Version 1.1");
+
+        System.out.println(project);
+        System.out.println();
+
+        System.out.println("Something went wrong...");
+        System.out.println("Rolling back to Version 1.0");
+
+        project.load(githubRepo.getSave());
+
+        System.out.println(project);
+        System.out.println();
+    }
+}
